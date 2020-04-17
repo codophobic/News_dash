d474acafab5578554989a9ca0a9565bb7e2714ca
ANKIT <mr.ankityo111@gmail.com>
2020-04-17 20:17:25 +0530

    Updates

---
+++ precache-manifest.4fb97a19e748a03f43a63c1f7c2c0d0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f09c9ab0c385af571f74ff355e5a976",
+    "url": "/News_dash/index.html"
+  },
+  {
+    "revision": "7eab3c6bf36f8c0afd4d",
+    "url": "/News_dash/static/css/2.599c45ea.chunk.css"
+  },
+  {
+    "revision": "2e356011ec730e0e881f",
+    "url": "/News_dash/static/css/main.d31f0cb4.chunk.css"
+  },
+  {
+    "revision": "7eab3c6bf36f8c0afd4d",
+    "url": "/News_dash/static/js/2.1cf05963.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/News_dash/static/js/2.1cf05963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e356011ec730e0e881f",
+    "url": "/News_dash/static/js/main.311b03a6.chunk.js"
+  },
+  {
+    "revision": "8d149e5b1a0845c89e13",
+    "url": "/News_dash/static/js/runtime-main.adf6814f.js"
+  },
+  {
+    "revision": "25f5b020e017a4ceadc0f0f8f110cc71",
+    "url": "/News_dash/static/media/nyt.25f5b020.png"
+  }
+]);
